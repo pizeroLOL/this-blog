266e2108ec29b61caf55a26b0e3327acd74430c7
Pizero <zhaory200707@outlook.com>
2025-04-20 18:27:40 +0800

    feat: basic seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://this-blog.pages.dev",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
