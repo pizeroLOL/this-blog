11d427102915f5fd290245c63abd25e29ce1d74f
Pizero <zhaory200707@outlook.com>
2025-05-10 02:52:19 +0800

    chore: switch domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://this-blog.pages.dev",
+  site:"https://www.pizero.top",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
